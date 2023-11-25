8096f9abe662cdbd47678668c52e969652b0c89b
CodingChar <cyberride14@gmail.com>
2023-11-25 11:18:51 -0500

    New functions added!

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,19 +3,47 @@ const socket = io();
 const display = document.querySelector(".display");
 const sendBtn = document.querySelector(".btn-send");
 
+function createUser(name){
+  if(!localStorage.getItem('chat-user')){
+    const randomCode = Math.floor(Math.random()*400)
+    let user = name+randomCode;
+    localStorage.setItem('chat-user', user)
+
+    alert(`Your user from now and on, Will be ${user}`)
+    return user;
+  }else{
+    return localStorage.getItem('chat-user')
+  }
+}
+
+socket.on('number-clients', data => {
+  document.querySelector('.server-info p').innerText ='Users Online: '+data;
+})
 
 socket.on("server-message", (data) => {
   console.log(data)
   displayMessage(data);
+  window.scrollTo(1, document.body.scrollHeight);
 });
 
 
 function message(e){
   e.preventDefault()
+
+
   let messageContent = document.querySelector('.chat-input')
+  
   console.log('click')
+  if(messageContent.value.length <= 0){
+    alert("No empty messages!")
+    return;
+  }else if(messageContent.value.length >= 40){
+    alert("Limit exceded, 40 characters or less")
+    return;
+  }
+  
   sendMessage({
-    username: "From Server",
+    username: createUser('User'),
     message: messageContent.value
   })
   messageContent.value = ""
--- socket.js
+++ socket.js
@@ -1,15 +1,28 @@
 const {io} = require('./index.js')
 
 
+let connections = 0
+
 io.on('connection', socket => {
+    connections+=1;
+
+    //Global emits
+    io.emit('number-clients', connections)
+
     console.log("User connected")
     console.log(socket)    
+    
+
     socket.on('chat-message', (data) => {
         console.log(data)
         io.emit("server-message", data)
     })
 
     socket.on('disconnect', () => {
+        connections-=1;
+
+        io.emit('number-clients', connections)
+        
         console.log("User disconnected")
     })
 })
